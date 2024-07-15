4346cd014c61aa052271748c283f029ea123b2f3
Ayush <ayushkansal303@gmail.com>
2024-07-15 16:15:30 +0530

    created auth for appwrite backend and usbale for multi backend as a services liek firebase. Just change the platform names , params remains the same

--- 09_BlogApp/src/conf/conf.js
+++ 09_BlogApp/src/conf/conf.js
@@ -1,4 +1,4 @@
-const config = {
+export default conf = {
     appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
     appwriteProjectId: String(import.meta.env.VITE_PROJECT_ID),
     appwriteDbName: String(import.meta.env.VITE_APPWRITE_DATABASE_NAME),
@@ -6,5 +6,3 @@ const config = {
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
 }
-
-export default config;
