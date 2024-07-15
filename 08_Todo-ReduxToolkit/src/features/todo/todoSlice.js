911762381d79d354830342fc0a0cbbb00b418b23
Ayush <ayushkansal303@gmail.com>
2024-07-15 12:34:49 +0530

    Built Todo App usign reduz toolkit. Implemented Configure store, reducers and integrated using Dispatch.

---
+++ 08_Todo-ReduxToolkit/src/features/todo/todoSlice.js
@@ -0,0 +1,26 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState: {
+        todos: []
+    },
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload
+                // "text: action.payload.text" : payload is object and 'text' key is there and it's being assigned to 'text' so due to same it's not required
+            }
+            state.todos.push(todo); // todos is defined in initial state
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        },
+        // uppdateTodo: (state, action) => { }
+    }
+})
+
+export const { addTodo, removeTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
--- Readme.md
+++ Readme.md
@@ -3,4 +3,5 @@ This repo is collection of mini projects i created while learning React Frontend
 
 ## Project Deployment
 1. Password Generator: [Live Demo](https://unique-password.vercel.app)
-2. Todo App: [Live Demo](https://keytasks.vercel.app)
+2. Todo using Context API: [Live Demo](https://keytasks.vercel.app)
+3. Todo using ReduxTookit: [Live Demo](https://.vercel.app)
